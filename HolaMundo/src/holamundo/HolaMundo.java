1a7d8dfca8c231b8ab8e109166e4f52436f50f63
bach <bach@203P12>
2022-01-20 11:05:09 +0100

    Segundo usuario

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -16,7 +16,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("soy  Rubik, gd player");
         System.out.println("segunda modificacion");
-        System.out.println("q hay");
+        System.out.println("Soy ELPEPE");
     }
     
 }
